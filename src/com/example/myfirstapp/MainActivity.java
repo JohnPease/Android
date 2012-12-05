aca5372d8370a577f81b014d929c65c0a601f713
John Pease <jpeaseninety@gmail.com>
2012-12-05 09:25:18 -0600

    added app project files

---
+++ src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    /* called when the user clicks the Send button */
+    public void sendMessage(View view) {
+    	/* 
+    	 * an intent is an object that provides a runtime binding between separate components (such as two activities)
+    	 * represents an app's "intent to do something"
+    	 * usually used to start another activity
+    	 * intents can carry a collection of data types as key-value pairs called extras
+    	 * putExtra takes the key name in the first parameter and the value in the second
+    	 * the key should somehow be related to your package name
+    	 * every activity has to be invoked by an Intent
+    	 */
+    	
+    	Intent intent;
+    	EditText editText 	= (EditText) findViewById(R.id.edit_message);
+    	String message 		= editText.getText().toString();
+    	
+    	if (message.equalsIgnoreCase("go to second activity")) {
+    		intent = new Intent(this, SecondActivity.class);
+    	} else {
+    		intent = new Intent(this, DisplayMessageActivity.class);
+    	}
+    	
+    	/* add the message into the intent */
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	
+    	/* start the activity */
+    	startActivity(intent);
+    	
+    	
+    }
+    
+}
---
+++ src/com/example/myfirstapp/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+
+		Intent intent = getIntent();
+		String message = "hit the second activity, message = " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(message);
+		
+		setContentView(textView);
+	}
+
+}
