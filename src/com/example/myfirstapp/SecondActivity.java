ae0d704093ce6cf54c55837311f47dd284d1d8d9
John Pease <jpeaseninety@gmail.com>
2012-12-24 08:54:08 -0600

    Finished spinner in second activity

--- src/com/example/myfirstapp/SecondActivity.java
+++ src/com/example/myfirstapp/SecondActivity.java
@@ -6,11 +6,15 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SecondActivity extends Activity {
+public class SecondActivity extends Activity implements OnItemSelectedListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +25,44 @@ public class SecondActivity extends Activity {
 		String entered = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 		String message = "you typed: " + entered;
 		
-		/*
-		TextView textView = new TextView(this);
-		textView.setTextSize(40);
-		textView.setText(message);
-		
-		setContentView(textView);
-		*/
-		
-		/* edit the text in the CenterLabel textview */
+		/* 
+		 * edit the text in the CenterLabel textview 
+		 */
 		TextView centerLabel = (TextView)findViewById(R.id.CenterLabel);
 		centerLabel.setText(message);
 		
 		/*
 		 * populate the spinner with the words typed
-		 * the code below probably doesn't work
-		 *
-		 *
-		
-		Spinner spin = (Spinner)findViewById(R.id.CenterSpinner);
-		
-		String enteredArray[] = entered.split(" ");
-		List<String> words = new ArrayList<String>();
-		
+		 */
+		Spinner spin 			= (Spinner)findViewById(R.id.CenterSpinner);
+		String enteredArray[] 	= entered.split(" ");
+		List<String> words 		= new ArrayList<String>();
+
+		/*
+		 * put all of the words entered into the array for the spinner
+		 * create the set of options for the spinner
+		 */
 		for (int i = 0; i < enteredArray.length; i++) words.add(enteredArray[i]);
 		
-		ArrayAdapter<String> options = new ArrayAdapter<String>(this, R.layout.activity_second, words);
+		ArrayAdapter<String> options = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, words);
+		options.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(options);
 		
-		*/
+		spin.setOnItemSelectedListener(this);
+		
+		//String chosen = String.valueOf(spin.getSelectedItem());
+		//Toast.makeText(this, "You selected \"" + chosen + "\" from the Spinner", Toast.LENGTH_SHORT);
 	}
+	
+	public void onItemSelected(AdapterView<?> parent, View view, 
+            int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+		Toast.makeText(this, "You selected \"" + parent.getItemAtPosition(pos) + "\" from the Spinner", Toast.LENGTH_SHORT);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
 
 }
